6f559e4b1f7c7d5af3b862ecd3a4c5dcbea44d2d
Linda Spellman <lindaspellman@live.com>
2021-11-13 12:58:20 -0700

    25% of the way

---
+++ Casting/Brick.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using cse210_batter_csharp.Casting;
+using cse210_batter_csharp.Services;
+using cse210_batter_csharp.Scripting;
+
+namespace cse210_batter_csharp
+{
+    /// <summary>
+    /// 
+    /// 
+    /// Stereotype:
+    ///     
+    /// </summary>
+    public class Brick : Actor 
+    {
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,29 @@ namespace cse210_batter_csharp
             // Bricks
             cast["bricks"] = new List<Actor>();
 
-            // TODO: Add your bricks here
+            Brick b1 = new Brick();
+            Point p1 = new Point(10, 10);
+            b1.SetPosition(p1);
+
+
+            Brick b2 = new Brick();
+            Point p2 = new Point(100, 10);
+            b2.SetPosition(p2);
+
+            Brick b3 = new Brick();
+            Point p3 = new Point(200, 10);
+            b3.SetPosition(p3); 
+
+            List<Brick> bricks = new List<Brick>();
+            bricks.Add(b1);
+            bricks.Add(b2);
+            bricks.Add(b3);
+
+            foreach (Brick b in bricks)
+            {
+                b.GetPosition(); //doesn't do anything
+                Console.WriteLine($"{b}: ({b.GetX()}, {b.GetY()})");
+            }
 
             // The Ball (or balls if desired)
             cast["balls"] = new List<Actor>();
